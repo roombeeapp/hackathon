aae550f8f1329fc44c2562b0dcd7d56ffde2a38c
Adwait <ganguly.adwait@gmail.com>
2023-10-07 11:17:31 -0500

    Initial Commit

---
+++ roombee/roombeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  roombeeApp.swift
+//  roombee
+//
+//  Created by Adwait Ganguly on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct roombeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
