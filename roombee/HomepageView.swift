54a49e617e679ae5bd4497e4e9ef44be1af834b5
Adwait <ganguly.adwait@gmail.com>
2023-10-07 12:38:35 -0500

    loading page initialized

---
+++ roombee/HomepageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomepageView.swift
+//  roombee
+//
+//  Created by Adwait Ganguly on 10/7/23.
+//
+
+import SwiftUI
+
+struct HomepageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomepageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomepageView()
+    }
+}
