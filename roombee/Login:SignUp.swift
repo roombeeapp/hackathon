6acf83a62b2f674d80d9d546446f0b157b5b960b
alizenart <97767073+alizenart@users.noreply.github.com>
2023-10-07 19:06:17 -0500

    Login Links to HomePage
    Login Links to HomePage

--- roombee/Login:SignUp.swift
+++ roombee/Login:SignUp.swift
@@ -7,83 +7,83 @@
 
 import SwiftUI
 
-
-
 struct SignUp: View {
     
     @State private var email = ""
     @State private var password = ""
     @State private var isSignUp = false
+    @State private var navigateToHomepage = false
     
-  
-
     var body: some View {
         
-        ZStack{
-            Color.purple
-                .ignoresSafeArea()
-            Circle()
-                .scale(1.7)
-                .foregroundColor(.white.opacity(0.15))
-            Circle()
-                .scale(1.35)
-                .foregroundColor(.white)
-            
-            
-            VStack{
-                if isSignUp {
-                    Text("Sign Up")
-                        .font(.largeTitle)
-                        .bold()
+        NavigationView {
+            ZStack{
+                Color.purple
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack{
+                    if isSignUp {
+                        Text("Sign Up")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding()
+                    } else {
+                        Text("Login")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding()
+                    }
+                    
+                    TextField("Email", text: $email)
                         .padding()
-                } else {
-                    Text("Login")
-                        .font(.largeTitle)
-                        .bold()
+                        .frame(width:300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    SecureField("Password", text: $password)
                         .padding()
-                }
-                
-                
-                TextField("Email", text: $email)
-                    .padding()
-                    .frame(width:300, height:50)
-                    .background(Color.black.opacity(0.05))
-                    .cornerRadius(10)
-                SecureField("Password", text: $password)
-                    .padding()
-                    .frame(width:300, height:50)
-                    .background(Color.black.opacity(0.05))
-                    .cornerRadius(10)
-                
-                
-                
-                Button(action: isSignUp ? signUp : login) {
-                    Text(isSignUp ? "Sign Up" : "Login")
-                }
-                
-                Button(action: {
-                    isSignUp.toggle()
-                }) {
-                    Text(isSignUp ? "Already have an account? Login!" : "Don't have an account? Sign Up!")
-                }
-                
-            }
-        }
-        .navigationBarHidden(true)
-        
-       
-    }
-    func signUp() {
-        
-        
-        }
+                        .frame(width:300, height:50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    
+                    Button(action: isSignUp ? signUp : login) {
+                        Text(isSignUp ? "Sign Up" : "Login")
+                    }
+                    
 
-    func login() {
-       
+                    
+                    Button(action: {
+                        isSignUp.toggle()
+                    }) {
+                        Text(isSignUp ? "Already have an account? Login!" : "Don't have an account? Sign Up!")
+                    }
+                }
+                NavigationLink(destination: HomepageView(calGrid: GridView(cal: CalendarView(title: String()), cal2: CalendarView(title: String()))), isActive: $navigateToHomepage) {
+                                    EmptyView()  // This won't display anything but is necessary for the programmatic navigation
+                                }
+            }
+            .navigationBarHidden(true)
+        }
     }
     
+    func signUp() {
+        // Your sign-up logic
+    }
+
+    func login() {
+        // Your login logic
+        // Navigate to HomepageView after a successful login
+        self.navigateToHomepage = true
+    }
 }
 
-#Preview {
-    SignUp()
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
 }
